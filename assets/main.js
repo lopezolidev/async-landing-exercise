cc2c105413aef32f4688cb298c5c06b3eaed7b30
rockwatch <rocksistor@gmail.com>
2022-10-26 19:04:36 -0400

    Updates

--- assets/main.js
+++ assets/main.js
@@ -33,12 +33,14 @@ async function fetchData(urlApi){
         <div class="group relative">
             <div
             class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                <a href="youtube.com/${video.snippet.channelId}"><img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full"></a>
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
             </div>
             <div class="mt-4 flex justify-between">
             <h3 class="text-sm text-gray-700">
+            <a href="https://www.youtube.com/c/thebootlegboy">
                 <span aria-hidden="true" class="absolute inset-0"></span>
                 ${video.snippet.title}
+                </a>
             </h3>
             </div>
         </div>
